2eab5eb9a8e14511b976e7c3a0b3addc6538f1ba
Anton Boyko <me@boykoant.pro>
2019-12-16 23:58:00 +0200

    started with face recognition service

---
+++ src/service/FaceService.ts
@@ -0,0 +1,103 @@
+import ConfigService from "./config/ConfigService";
+
+import {HttpMethod, HttpService} from "./helper/HttpService";
+import {DetectionModel, PersonGroupModel} from "../application/model/PersonGroup/PersonGroupModel";
+import {PersonGroupTrainingStatusModel} from "../application/model/PersonGroup/PersonGroupTrainingStatusModel";
+import {PersonModel} from "../application/model/Person/PersonModel";
+import {DetectFaceModel} from "../application/model/Face/DetectFaceModel";
+import {RectangleModel} from "../application/model/Face/Base/RectangleModel";
+import {IdentifyModel} from "../application/model/Identify/IdentifyModel";
+
+export default class FaceService {
+    public static async listPersonGroupsAsync(): Promise<PersonGroupModel[]> {
+        return await this.client.callApiAsync(`/persongroups`);
+    }
+
+    public static async createPersonGroupAsync(groupId: string, model: PersonGroupModel): Promise<void> {
+        await this.client.callApiAsync(`/persongroups/${groupId}`, HttpMethod.PUT, model);
+    }
+
+    public static async trainPersonGroupAsync(groupId: string): Promise<void> {
+        await this.client.callApiAsync(`/persongroups/${groupId}/train`, HttpMethod.POST);
+    }
+
+    public static async checkPersonGroupTrainingStatusAsync(groupId: string): Promise<PersonGroupTrainingStatusModel> {
+        return await this.client.callApiAsync(`/persongroups/${groupId}/training`);
+    }
+
+    public static async listPersonsAsync(groupId: string): Promise<PersonModel[]> {
+        return await this.client.callApiAsync(`/persongroups/${groupId}/persons`);
+    }
+
+    public static async createPersonAsync(groupId: string, model: PersonModel): Promise<PersonModel> {
+        const res = await this.client.callApiAsync(`/persongroups/${groupId}/persons`, HttpMethod.POST, model);
+
+        model.personId = res.personId;
+        delete model.persistedFaceIds;
+
+        return model;
+    }
+
+    public static async addPersonFaceAsync(groupId: string, personId: string, url: string, faceRectangle: RectangleModel): Promise<void> {
+        const coordinates = [
+            faceRectangle.left,
+            faceRectangle.top,
+            faceRectangle.width,
+            faceRectangle.height
+        ].join(",");
+
+        const query = [
+            `detectionModel=${DetectionModel.V2}`,
+            `targetFace=${coordinates}`
+        ].join("&");
+
+        return await this.client.callApiAsync(
+            `/persongroups/${groupId}/persons/${personId}/persistedFaces?${query}`,
+            HttpMethod.POST,
+            { url });
+    }
+
+    public static async detectFacesAsync(url: string): Promise<DetectFaceModel[]> {
+        const attributes = [
+            "age",
+            "gender",
+            "headPose",
+            "smile",
+            "facialHair",
+            "glasses",
+            "emotion",
+            "hair",
+            "makeup",
+            "occlusion",
+            "accessories",
+            "blur",
+            "exposure",
+            "noise",
+        ].join(",");
+
+        const query = [
+            `returnFaceId=${true}`,
+            `returnFaceLandmarks=${true}`,
+            `returnFaceAttributes=${attributes}`,
+            `recognitionModel=${ConfigService.Face.RecognitionModel}`,
+            `returnRecognitionModel=${true}`,
+            `detectionModel=${ConfigService.Face.DetectionModel}`,
+        ].join("&");
+
+        return await this.client.callApiAsync(
+            `/detect?${query}`,
+            HttpMethod.POST,
+            { url });
+    }
+
+    public static async identifyFacesAsync(faceIds: string[], personGroupId: string): Promise<IdentifyModel[]> {
+        return await this.client.callApiAsync(
+            "/identify",
+            HttpMethod.POST,
+            { faceIds, personGroupId });
+    }
+
+    private static client = new HttpService(ConfigService.Face.Url, {
+        "Ocp-Apim-Subscription-Key": ConfigService.Face.Key,
+    });
+}
